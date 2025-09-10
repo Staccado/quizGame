07cc2ea3a834188de029621699bb13499df4ead0
Chris <chrismartel93@gmail.com>
2025-09-10 12:11:51 -0400

    pictionary + UI fixes

--- react-jeopardy/src/QuestionDisplay.js
+++ react-jeopardy/src/QuestionDisplay.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import DrawingBoard from './drawing';
+import { SocketContext } from './SocketContext';
 
-export function QuestionDisplay({ questionText, questionImage }) {
-    return (
-        <div className="question-content">
-            {questionImage && <img src={questionImage} alt="Question" className="question-image" />}
-            <div className="question-text">
-                {questionText}
+export function QuestionDisplay({ questionText, questionImage, drawingBoard, lastCorrectPlayer, pictionaryImageUrl, pictionarySubmittedBy }) {
+    const socket = useContext(SocketContext);
+    const [playerID, setPlayerID] = useState(null);
+
+    useEffect(() => {
+        setPlayerID(socket.id);
+    }, [socket]);
+
+    const handleImageSubmit = (image) => {
+        console.log('Submitting pictionary image');
+        socket.emit('pictionaryImageSubmitted', image);
+    };
+
+    if (!drawingBoard) {
+        return (
+            <div className="question-content">
+                {questionImage && <img src={questionImage} alt="Question" className="question-image" />}
+                <div className="question-text">
+                    {questionText}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    if (drawingBoard) {
+        // Check if current player is the one who should draw
+        if (lastCorrectPlayer === playerID) {
+            return (
+                <div className="question-content">
+                    <div className="question-text">
+                        {questionText}
+                    </div>
+                    <DrawingBoard onSubmit={handleImageSubmit} />
+                </div>
+            );
+        } else {
+            // Other players wait for the image
+            return (
+                <div className="question-content">
+                    <div className="question-text">
+                        {questionText}
+                    </div>
+                    {pictionaryImageUrl ? (
+                        <img src={pictionaryImageUrl} alt="Pictionary Drawing" className="question-image" />
+                    ) : (
+                        <div className="waiting-message">
+                            Waiting for drawing...
+                        </div>
+                    )}
+                </div>
+            );
+        }
+    }
 }
+
+export default QuestionDisplay;
